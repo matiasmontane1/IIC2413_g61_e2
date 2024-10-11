1a845531f2a73a4a6d6a1c99d99e2ab04dafc92a
Cristóbal Muñoz <cmum@uc.cl>
2024-10-11 13:09:22 -0300

    cargador y xlsx datos

---
+++ cargador.php
@@ -0,0 +1,11 @@
+
+$host = "localhost"
+$dbname = "database_name"
+$user = "username"
+$password = "password"
+
+$conn = pg_connect("host=$host dbname=$dbname user=$user password=$password")
+
+if (!$conn) {
+    die("Connection failed:" . pg_last_error())
+}
