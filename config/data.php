061a4370906aa117b24758f28541f15fce272cc2
IÑAKI URIARTE <iuriarte@uc.cl>
2024-10-11 13:07:15 -0300

    partiendo cargadores

---
+++ config/data.php
@@ -0,0 +1,5 @@
+<?php
+  $user = 'inaki'; // acá va su grupo, ejemplo grupoXX
+  $password = 'Vasco.17'; // acá la contraseña del grupo
+  $databaseName = 'grupo61'; // acá la base de datos debe llamarse así: grupoXX
+?>
