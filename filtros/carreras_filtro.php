0fce2cf27315dfc58ea1f59eaa9c976c12313974
matiasmontane1 <matiasmontane1@uc.cl>
2024-10-14 12:16:02 -0300

    fyc

---
+++ filtros/carreras_filtro.php
@@ -0,0 +1,47 @@
+<?php
+require "./funciones_filtro.php";
+
+$archivo_datos = fopen("../datos_malos/Carreras_bad.csv", "r");
+$array_datos = [];
+
+$columnas_encabezado_modificado = [
+    "NombreCarrera",
+];
+
+$array_datos[] = $columnas_encabezado_modificado;
+
+while (!feof($archivo_datos)) {
+    $linea = fgets($archivo_datos);
+
+    if (hay_datos($linea)) {
+        $columnas = explode("|", $linea);
+        
+        $nombre = no_nulo($columnas[0]);
+
+        if ($nombre) {
+            $columnas_seleccionadas = [
+                $nombre
+            ];
+            $array_datos[] = $columnas_seleccionadas;
+        }
+    }
+}
+fclose($archivo_datos); 
+
+$array_datos_buenos = pk_unica($array_datos, 0);
+
+$archivo_datos = fopen("../datos_aceptados/Carreras_gud.csv", "w");
+
+$total_filas = count($array_datos_buenos);
+foreach ($array_datos_buenos as $index => $dato) {
+    $linea = implode(";", $dato);
+    
+    if ($index < $total_filas - 1) {
+        fwrite($archivo_datos, trim($linea) . PHP_EOL);
+    } else {
+        fwrite($archivo_datos, trim($linea));
+    }
+}
+
+fclose($archivo_datos);
+?>
---
+++ filtros/funciones_filtro.php
@@ -0,0 +1,76 @@
+<?php
+
+function no_nulo($dato) {
+    if (isset($dato) && $dato !== '') {
+        return trim($dato);
+    } else {
+        return null;
+    }
+}
+
+function no_nulo_int($dato) {
+    if (isset($dato) && filter_var($dato, FILTER_VALIDATE_INT) !== false) {
+        return (int)$dato;
+    }
+    return null;
+}
+
+function pk_unica($array_datos, $columna_pk) {
+    $valores_vistos = [];
+    $resultado = [];
+    foreach ($array_datos as $tupla) {
+        if (!in_array($tupla[$columna_pk], $valores_vistos)) {
+            $resultado[] = $tupla;
+            $valores_vistos[] = $tupla[$columna_pk];
+        }
+    }
+    return $resultado;
+}
+
+function hay_datos($linea) {
+    $linea_limpia = trim($linea);
+    if (!empty($linea_limpia)) {
+        return true;
+    }
+    return false;
+}
+
+function fecha($dato) {
+    $fecha = DateTime::createFromFormat('dd/mm/yy', $dato);
+    if ($fecha && $fecha->format('dd/mm/yy') === $dato) {
+            return $dato;
+    }
+    return null;
+}
+
+function jornada($dato) {
+    $dato_limpio = strtoupper(trim($dato));
+    if ($dato_limpio === "DIURNA" || $dato_limpio === "VESPERTINA") {
+            return $dato_limpio;
+    }
+    return null;
+}
+
+function modalidad($dato) {
+    $dato_limpio = strtoupper(trim($dato));
+    if ($dato_limpio === "PRESENCIAL" || $dato_limpio === "ONLINE" || $dato_limpio === "HÍBRIDA") {
+            return $dato_limpio;
+    }
+    return null;
+}
+
+function sede($dato) {
+    $dato_limpio = strtoupper(trim($dato));
+    if ($dato_limpio === "HOGWARTS" || $dato_limpio === "BEAUXBATON" || $dato_limpio === "UAGADOU") {
+        return $dato_limpio;
+    }
+    return null;
+}
+
+function grado($dato) {
+    $dato_limpio = strtoupper(trim($dato));
+    if ($dato_limpio === "PROGRAMA ESPECIAL DE LICENCIATURA" || $dato_limpio === "PREGRADO" || $dato_limpio === "POSTGRADO") {
+        return $dato_limpio;
+    }
+    return null;
+}
