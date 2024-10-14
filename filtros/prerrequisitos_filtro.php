a64724b417f83a6a10590c6d8d50266de56e68e1
matiasmontane1 <matiasmontane1@uc.cl>
2024-10-14 18:01:46 -0300

    pr

---
+++ filtros/prerrequisitos_filtro.php
@@ -0,0 +1,52 @@
+<?php
+require "./funciones_filtro.php";
+
+$archivo_datos = fopen("../datos_malos/Prerequisitos_bad.csv", "r");
+$array_datos = [];
+
+$columnas_encabezado_modificado = [
+    "Sigla"
+];
+
+$array_datos[] = $columnas_encabezado_modificado;
+
+while (!feof($archivo_datos)) {
+    $linea = fgets($archivo_datos);
+
+    if (hay_datos($linea)) {
+        $columnas = explode("|", $linea);
+
+        $sigla = no_nulo($columnas[0]);
+        $req1 = default_str($columnas[1]);
+        $req2 = default_str($columnas[2]);
+
+
+        if ($sigla) {
+            $columnas_seleccionadas = [
+                $sigla,
+                $req1,
+                $req2
+            ];
+            $array_datos[] = $columnas_seleccionadas;
+        }
+    }
+}
+fclose($archivo_datos); 
+
+$array_datos_buenos = agregar_id($array_datos, "PrerrequisitosID");
+
+$archivo_datos = fopen("../datos_aceptados/Prerequisitos_gud.csv", "w");
+
+$total_filas = count($array_datos_buenos);
+foreach ($array_datos_buenos as $index => $dato) {
+    $linea = implode(";", $dato);
+    
+    if ($index < $total_filas - 1) {
+        fwrite($archivo_datos, trim($linea) . PHP_EOL);
+    } else {
+        fwrite($archivo_datos, trim($linea));
+    }
+}
+
+fclose($archivo_datos);
+?>
